67cafa4d67628b5db9249e07db14e2b51c9c3168
Hawii02 <leon.onditi@student.moringaschool.com>
2024-02-05 22:53:21 +0300

    COmpleted

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,32 +1,55 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
-function Question({ question, onAnswered }) {
+function Question({ onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
   // add useEffect code
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      setTimeRemaining((prevTimeRemaining) => prevTimeRemaining - 1);
+    }, 1000);
 
-  function handleAnswer(isCorrect) {
-    setTimeRemaining(10);
-    onAnswered(isCorrect);
-  }
+    if (timeRemaining === 0) {
+      setTimeRemaining(10);
+      onAnswered(false);
+    }
 
-  const { id, prompt, answers, correctIndex } = question;
+    return () => {
+      clearTimeout(timeoutId);
+    };
+  }, [timeRemaining, onAnswered]);
+
+  
+  // function handleAnswer(isCorrect) {
+  //   setTimeRemaining(10);
+  //   onAnswered(isCorrect);
+  // }
+
+  // const { id, prompt, answers, correctIndex } = question;
 
   return (
-    <>
-      <h1>Question {id}</h1>
-      <h3>{prompt}</h3>
-      {answers.map((answer, index) => {
-        const isCorrect = index === correctIndex;
-        return (
-          <button key={answer} onClick={() => handleAnswer(isCorrect)}>
-            {answer}
-          </button>
-        );
-      })}
-      <h5>{timeRemaining} seconds remaining</h5>
-    </>
+    <div>
+      <p>Time remaining: {timeRemaining}</p>
+      <p>Question goes here</p>
+    </div>
   );
 }
+
+  // return (
+  //   <>
+  //     <h1>Question {id}</h1>
+  //     <h3>{prompt}</h3>
+  //     {answers.map((answer, index) => {
+  //       const isCorrect = index === correctIndex;
+  //       return (
+  //         <button key={answer} onClick={() => handleAnswer(isCorrect)}>
+  //           {answer}
+  //         </button>
+  //       );
+  //     })}
+  //     <h5>{timeRemaining} seconds remaining</h5>
+  //   </>
+  // );
+
 
 export default Question;
