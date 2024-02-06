4992928a69a5a9363faf31918a1bde5ee7a14f48
Hawii02 <leon.onditi@student.moringaschool.com>
2024-02-06 11:11:02 +0300

    changes made

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 
-function Question({ onAnswered }) {
+function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
   // add useEffect code
@@ -13,43 +13,42 @@ function Question({ onAnswered }) {
       setTimeRemaining(10);
       onAnswered(false);
     }
-
+ // cleanup function
     return () => {
       clearTimeout(timeoutId);
     };
   }, [timeRemaining, onAnswered]);
 
-  
-  // function handleAnswer(isCorrect) {
-  //   setTimeRemaining(10);
-  //   onAnswered(isCorrect);
-  // }
+useEffect(()=>{
+    if (timeRemaining === 0){
 
-  // const { id, prompt, answers, correctIndex } = question;
+  setTimeRemaining(10);
+  onAnswered(false)
+}
+  },[timeRemaining,onAnswered])
+
+  function handleAnswer(isCorrect) {
+    setTimeRemaining(10);
+    onAnswered(isCorrect);
+  }
+
+  const { id, prompt, answers, correctIndex } = question;
 
   return (
-    <div>
-      <p>Time remaining: {timeRemaining}</p>
-      <p>Question goes here</p>
-    </div>
+    <>
+      <h1>Question {id}</h1>
+      <h3>{prompt}</h3>
+      {answers.map((answer, index) => {
+        const isCorrect = index === correctIndex;
+        return (
+          <button key={answer} onClick={() => handleAnswer(isCorrect)}>
+            {answer}
+          </button>
+        );
+      })}
+      <h5>{timeRemaining} seconds remaining</h5>
+    </>
   );
 }
-
-  // return (
-  //   <>
-  //     <h1>Question {id}</h1>
-  //     <h3>{prompt}</h3>
-  //     {answers.map((answer, index) => {
-  //       const isCorrect = index === correctIndex;
-  //       return (
-  //         <button key={answer} onClick={() => handleAnswer(isCorrect)}>
-  //           {answer}
-  //         </button>
-  //       );
-  //     })}
-  //     <h5>{timeRemaining} seconds remaining</h5>
-  //   </>
-  // );
-
 
 export default Question;
